7a8aedb49a5243dbfaf7204f36ff5ad2d67be906
Kalei Bih <hubbard_raimon@yahoo.com>
2021-04-02 15:00:46 -0500

    Added Arrays

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -1,1 +1,20 @@
-console.log("hellothere");
+"use strict"
+//The code will generate a random destination, restaurant, mode of
+//transportation, and a form of entertainment to be slected for a day 
+//trip. My first step will be to create arrays to randomly select from.
+
+let destination = [];
+destination.push("Detroit", "Ann Arbor", "Lansing", "Troy", "Traverse City", 
+"Chicago", "Romoulus");
+
+let restaurants = [];
+restaurants.push("Capers","White Castle", "Golden Coral", "Taco Bell", "Kow Loons",
+"Red Lobster", "Steak and Shake");
+
+let transportation = [];
+transportation.push("Personal Vehicle", "Rideshare", "Train", "Taxi", "Rental");
+
+let entertainment = []
+entertainment.push("Tour a History Museum", "Go on a City Tour", "Go Fishing", 
+"See a Play", "Go to the Arcade", "Check out the Local Shopping", "Get Some Drinks");
+
